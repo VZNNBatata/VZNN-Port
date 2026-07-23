b4a7d846c2c9172f2c28f9a2361bf9e0c99e2584
VZNNBatata <victoraraujopessoal080210@gmail.com>
2026-07-22 21:56:10 -0300

    Fix indentation in main.jsx for BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import "@/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-   <BrowserRouter basename={import.meta.env.BASE_URL}>
-  <App />
-</BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
